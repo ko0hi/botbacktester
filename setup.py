ae862bb2dc06791f2963bb4809035b89bd9e3481
ko0hi <ryosuke.kohita.bit@gmail.com>
2022-06-10 14:53:21 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['botbacktester', 'botbacktester.fast']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.5.2,<4.0.0',
+ 'numpy>=1.22.3,<2.0.0',
+ 'pandas>=1.4.2,<2.0.0',
+ 'scipy>=1.8.0,<2.0.0',
+ 'tqdm>=4.64.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'botbacktester',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'ko0hi',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
